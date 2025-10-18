4cc0a8aff5ce730cdb48e1442ef775a2ab390618
ferret22 <84383828+ferret22@users.noreply.github.com>
2025-10-18 19:52:55 +0000

    post and patch

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from models import init_db
 import requests as rq
 
 
+class AddTask(BaseModel):
+    tg_id: int
+    title: str
+
+
+class CompleteTask(BaseModel):
+    id: int
+
+
 @asynccontextmanager
 async def lifespan(app_: FastAPI):
     await init_db()
@@ -37,3 +46,16 @@ async def profile(tg_id: int):
     user = await rq.add_user(tg_id)
     completed_tasks_count = await rq.get_completed_tasks_count(user.id)
     return {'completedTasks': completed_tasks_count}
+
+
+@app.post("/api/add")
+async def add_task(task: AddTask):
+    user = await rq.add_user(task.tg_id)
+    await rq.add_task(user.id, task.title)
+    return {'status': 'OK'}
+
+
+@app.patch("/api/completed")
+async def complete_task(task: CompleteTask):
+    await rq.update_task(task.id)
+    return {'status': 'OK'}
--- requests.py
+++ requests.py
@@ -42,3 +42,19 @@ async def get_tasks(user_id: int):
 async def get_completed_tasks_count(user_id: int):
     async with async_session() as session:
         return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+
+
+async def add_task(user_id: int, title: str):
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
+
+async def update_task(task_id: int):
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).value(completed=True))
+        await session.commit()
